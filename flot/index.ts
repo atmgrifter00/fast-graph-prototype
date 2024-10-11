35ae0db7ec77f5988637c678f273f19fbc08e736
Jonathan Meyer <jonathan.meyer@ni.com>
2024-10-11 15:34:39 -0500

    More performance reporting for flot

--- flot/index.ts
+++ flot/index.ts
@@ -12,17 +12,20 @@ const d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
 // A null signifies separate line segments
 
 const d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
-const flot = $.plot(flotElement, [], { yaxis: { min: -2, max: 12, autoScale: 'none', }, xaxis: { min: 0, max: SAMPLE_SIZE }, zoom: { interactive: true }, pan: { interactive: true } });
+const flot = $.plot(flotElement, [], { yaxis: { min: -3, max: 12, autoScale: 'none', }, xaxis: { min: 0, max: SAMPLE_SIZE }, zoom: { interactive: true }, pan: { interactive: true } });
 const timeElement = document.getElementById('time_ms')!;
+const dataPerSecElement = document.getElementById('data_per_sec')!;
 let plotIndex = 0;
 let totalPlotTime = 0;
 let totalRenders = 0;
+let totalData = 0;
+let currentTime = 0;
+let previousTime = 0;
 
 window.setInterval(() => {
 	const plotData = tableIPCData[plotIndex];
 	const table = tableFromIPC(plotData);
 	const yData = table.data[0]?.children[0]?.values as Float32Array;
-	const xData = table.data[0]?.children[1]?.values as Int32Array;
 	const flotData = (() => {
 		const data = [];
 		for (let i = 0; i < CHANNELS ; i++) {
@@ -33,13 +36,19 @@ window.setInterval(() => {
 		}
 		return data;
 	})();
-    const currentTime = performance.now();
+    currentTime = performance.now();
+	previousTime = previousTime === 0 ? currentTime : previousTime;
 	flot.setData(flotData);
 	flot.setupGrid();
 	flot.draw();
+	totalData += SAMPLE_SIZE * CHANNELS;
 	totalPlotTime += performance.now() - currentTime;
     totalRenders++;
     timeElement.innerText = `Average render time: ${(totalPlotTime / totalRenders).toFixed(2)} ms`;
+	if (currentTime - previousTime > 1000) {
+		dataPerSecElement.innerText = `Data per second: ${(totalData / (totalPlotTime / 1000)).toFixed(2)}`;
+		previousTime = currentTime;
+	}
 	plotIndex = (plotIndex + 1) % 4;
 }, 0);
 
